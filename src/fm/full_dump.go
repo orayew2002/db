d08b0ef70127e6356d710ecf1e74ed1fb44cafc3
orayew2002 <serdarorayew2002@gmail.com>
2026-05-20 23:28:31 +0500

    lts

--- src/fm/full_dump.go
+++ src/fm/full_dump.go
@@ -25,7 +25,7 @@ func (f *FmFullDump) Flush(data any) error {
 	}
 
 	tmpPath := f.path + "_tmp"
-	if err := os.WriteFile(tmpPath, b, 0644); err != nil {
+	if err := os.WriteFile(tmpPath, b, 0o644); err != nil {
 		return err
 	}
 
